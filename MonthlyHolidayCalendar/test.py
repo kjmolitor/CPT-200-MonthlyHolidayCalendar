db2eb06cde75393ef09adc829a361dc12f57eb5c
Montana Shaw <mshaw788@gmail.com>
2022-07-05 21:14:31 -0500

    Started the GUI layout, have the basic setup for the events. I am currently stuck on getting the month forward button and month backward button

--- MonthlyHolidayCalendar/test.py
+++ MonthlyHolidayCalendar/test.py
@@ -1,2 +1,18 @@
+
+from tkinter import *
+
+root = Tk()
+
+def myClick():
+    # Creates a button
+    myButton = root.Button(root, text="Sign in", command=myClick)
+    myButton.pack()
+
+     #Creates a label widget
+    myLabel = Label(root, text="My Calendar")
+    nxLabel = Label(root, text="Please Sign in")
+    myLabel.pack()
+    nxLabel.pack()
+root.mainloop()
 if __name__ == "__main__":
     print("Helo test")
